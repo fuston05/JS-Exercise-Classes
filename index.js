391ab661f80cd18308897e8f78e3fe7fd88f5abc
fuston05 <fuston05@yahoo.com>
2019-11-07 11:36:39 -0500

    Passed 'Student' subClass

--- index.js
+++ index.js
@@ -176,9 +176,26 @@ class Instructor extends Lambdasian{
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian{
+  constructor( { name, age, location, previousBackground, className, favSubjects } ){
+    super({name, age, location});
+    this.previousBackground= previousBackground;
+    this. className= className;
+    this.favSubjects= favSubjects;
+  }//end constructor
+  
+  listSubjects(){
+    return `Loving ${this.favSubjects}!`;
+  }
 
-}
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
+}// end Student
 
 /*
   TASK 6
